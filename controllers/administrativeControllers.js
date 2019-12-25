a2faf60e0e7a14e3b3d6972e0a6e2eaf4b76f34f
Luthfi Aji W <luthfi.ajiw@gmail.com>
2019-12-25 13:18:13 +0700

    add pagination to cities

--- controllers/administrativeControllers.js
+++ controllers/administrativeControllers.js
@@ -38,16 +38,29 @@ module.exports = function (app) {
 
 	// get all cities
 	app.get('/api/v1/cities', checkAuth, (req, res, next) => {
-		City.findAll({
+		const { page, per_page } = req.query;
+
+		let _page = page === undefined ? 1 : parseInt(page, 10);
+		let perPage = per_page === undefined ? 10 : parseInt(per_page, 10);
+
+		City.findAndCountAll({
 			attributes: {exclude: ['createdAt', 'updatedAt', 'provinceId']},
 			include: [{
 				model: Province,
 				as: 'province',
 				attributes: ['id', 'name']
-			}]
+			}],
+			limit: perPage,
+			offset: (_page - 1) * 10
 		})
 			.then(cities => {
-				responseHandler(res, cities);
+				const totalPage = Math.ceil(cities.count/perPage);
+				const pagination = paginationHandler(_page, perPage, totalPage);
+				const datas = {
+					...cities,
+					...pagination,
+				}
+				responseHandler(res, datas);
 			})
 			.catch(err => {
 				errorHandler(500, err, res);
@@ -57,22 +70,33 @@ module.exports = function (app) {
 	// get cities based on province id
 	app.get('/api/v1/provinces/:provinceId/cities', checkAuth, (req, res, next) => {
 		const { provinceId } = req.params;
+		const { page, per_page } = req.query;
 
-		City.findAll({
+		let _page = page === undefined ? 1 : parseInt(page, 10);
+		let perPage = per_page === undefined ? 10 : parseInt(per_page, 10);
+
+		City.findAndCountAll({
 			where: {provinceId},
 			attributes: {exclude: ['createdAt', 'updatedAt', 'provinceId']},
 			include: [{
 				model: Province,
 				as: 'province',
 				attributes: ['id', 'name']
-			}]
+			}],
+			limit: perPage,
+			offset: (_page - 1) * 10
 		})
 			.then(cities => {
 				if (cities == null) {
 					errorHandler(404, `City with province id ${provinceId} can not be found.`, res);
 				}
-
-				responseHandler(res, cities);
+				const totalPage = Math.ceil(cities.count/perPage);
+				const pagination = paginationHandler(_page, perPage, totalPage);
+				const datas = {
+					...cities,
+					...pagination,
+				}
+				responseHandler(res, datas);
 			});
 	});
 };
