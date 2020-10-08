c10b0a2bafa8ebe264b4763d1436e9e373af2d15
Miko <41611726+Miko2x@users.noreply.github.com>
2020-10-08 10:26:39 +0700

    Add data subdistricts
    Add subdistricts in kab. bandung

--- data/subdistricts.js
+++ data/subdistricts.js
@@ -1,3 +1,14 @@
 let subdistricts = [
-  
+  {
+    "id":3204,
+    "provinceId":32,
+    "name":"Kab. Bandung",
+    daftarKecamatan: [
+      "1": "Kecamatan Batununggal"
+      "2": "Kecamatan Lengkong"
+      "3": "Kecamatan Kiaracondong"
+      "4": "Kecamatan Coblong"
+      "5": "Kecamatan Cicendo"
+    ]
+  }
 ];
